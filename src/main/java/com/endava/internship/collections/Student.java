bef72bd90351b54d741663b0196b8c4e3b8a0d25
dani3lz <danu10024@gmail.com>
2022-09-15 16:35:05 +0300

    created own TreeMap

--- src/main/java/com/endava/internship/collections/Student.java
+++ src/main/java/com/endava/internship/collections/Student.java
@@ -1,12 +1,11 @@
 package com.endava.internship.collections;
 
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.TreeMap;
 
-/**
- * The class that defines the element that will be contained by your collection
- */
-public class Student //TODO consider implementing any interfaces necessary for your collection
-{
+public class Student implements Comparable<Student> {
     private String name;
     private LocalDate dateOfBirth;
     private String details;
@@ -17,16 +16,43 @@ public class Student //TODO consider implementing any interfaces necessary for y
         this.details = details;
     }
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    public LocalDate getDateOfBirth() { return dateOfBirth; }
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
 
-    public String getDetails() { return details; }
+    public String getDetails() {
+        return details;
+    }
 
-    /*
-    TODO consider overriding any methods for this object to function properly within a collection:
-        1. A student is considered unique by a combination of their name and dateOfBirth
-        2. Student names are sorted alphabetically, if two students have the same name, then the older one is
-        placed before the younger student in an ordered student list.
-    */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return name.equals(student.name) && dateOfBirth.equals(student.dateOfBirth);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, dateOfBirth);
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return Comparator.comparing(Student::getName).
+                thenComparing(Student::getDateOfBirth).compare(this, student);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", details='" + details + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/endava/internship/collections/StudentMap.java
+++ src/main/java/com/endava/internship/collections/StudentMap.java
@@ -1,78 +1,199 @@
 package com.endava.internship.collections;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class StudentMap implements Map<Student, Integer> {
+    private int size;
+    private Node parent;
+    private Node found;
+
     @Override
     public int size() {
-        //TODO
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        //TODO
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean containsKey(Object o) {
-        //TODO
-        return false;
+        return foundNode(parent, (Student) o) != null;
     }
 
     @Override
     public boolean containsValue(Object o) {
-        //TODO
-        return false;
+        return findNodeValue(parent, (Integer) o);
     }
 
     @Override
     public Integer get(Object o) {
-        //TODO
-        return null;
+        Student student = (Student) o;
+        if (containsKey(student)) {
+            Node node = foundNode(parent, student);
+            found = null;
+            return node.value;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Integer put(Student student, Integer integer) {
-        //TODO
-        return null;
+        if (containsKey(student)) {
+            Node node = foundNode(parent, student);
+            found = null;
+            Integer oldValue = node.value;
+            node.set(student, integer);
+            return oldValue;
+        } else {
+            parent = insert(parent, student, integer);
+            size++;
+            return null;
+        }
     }
 
     @Override
     public Integer remove(Object o) {
-        //TODO
-        return null;
+        Student student = (Student) o;
+        if (containsKey(student)) {
+            Integer value = foundNode(parent, student).value;
+            found = null;
+            parent = deleteNode(parent, student);
+            size--;
+            return value;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public void putAll(Map<? extends Student, ? extends Integer> map) {
-        //TODO
+        for (Student s : map.keySet()) {
+            put(s, map.get(s));
+        }
     }
 
     @Override
     public void clear() {
-        //TODO
+        size = 0;
+        parent = null;
     }
 
     @Override
     public Set<Student> keySet() {
-        //TODO
-        return null;
+        Set<Student> students = new LinkedHashSet<>();
+        getKey(parent, students);
+        return students;
     }
 
     @Override
     public Collection<Integer> values() {
-        //TODO
-        return null;
+        Collection<Integer> values = new ArrayList<>();
+        getValue(parent, values);
+        return values;
     }
 
     @Override
     public Set<Entry<Student, Integer>> entrySet() {
         //Ignore this for homework
         throw new UnsupportedOperationException();
+    }
+
+    private void getKey(Node parent, Set<Student> students) {
+        if (parent != null) {
+            getKey(parent.left, students);
+            students.add(parent.key);
+            getKey(parent.right, students);
+        }
+    }
+
+    private void getValue(Node parent, Collection<Integer> values) {
+        if (parent != null) {
+            getValue(parent.left, values);
+            values.add(parent.value);
+            getValue(parent.right, values);
+        }
+    }
+
+    private Node foundNode(Node parent, Student student) {
+        if (parent == null) {
+            return null;
+        }
+        if (student.compareTo(parent.key) == 0) {
+            found = parent;
+            return found;
+        } else if (foundNode(parent.left, student) != null || foundNode(parent.right, student) != null) {
+            return found;
+        }
+        return null;
+    }
+
+    private boolean findNodeValue(Node parent, Integer value) {
+        if (parent == null) {
+            return false;
+        } else if (Objects.equals(value, parent.value)) {
+            return true;
+        } else if (findNodeValue(parent.left, value)) {
+            return true;
+        } else {
+            return findNodeValue(parent.right, value);
+        }
+    }
+
+    private Node insert(Node parent, Student student, Integer value) {
+        if (parent == null) {
+            parent = new Node(student, value);
+        }
+        int result = student.compareTo(parent.key);
+        if (result < 0) {
+            parent.left = insert(parent.left, student, value);
+        } else if (result > 0) {
+            parent.right = insert(parent.right, student, value);
+        }
+        return parent;
+    }
+
+    private Node deleteNode(Node parent, Student student) {
+        if (parent == null) {
+            return null;
+        }
+        int result = student.compareTo(parent.key);
+        if (result < 0) {
+            parent.left = deleteNode(parent.left, student);
+        } else if (result > 0) {
+            parent.right = deleteNode(parent.right, student);
+        } else {
+            if (parent.left == null) {
+                return parent.right;
+            } else if (parent.right == null) {
+                return parent.left;
+            }
+            parent.key = parent.right.key;
+            while (parent.right.left != null) {
+                parent.key = parent.right.left.key;
+            }
+            parent.right = deleteNode(parent.right, parent.key);
+        }
+        return parent;
+    }
+
+    class Node {
+        Student key;
+        Integer value;
+        Node left;
+        Node right;
+
+        public Node(Student key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        void set(Student student, Integer integer) {
+            key = student;
+            value = integer;
+        }
     }
 }
 
